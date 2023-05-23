db3a2c0d5555f9ea36736bcfc89bd1c75c1d7499
tardisgallifrey <bigmanrunning@gmail.com>
2023-05-23 18:38:20 -0500

    Vue houses 05/23/2023

--- projecthouse/vue/houses/src/App.vue
+++ projecthouse/vue/houses/src/App.vue
@@ -1,47 +1,65 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h1>A Vue app for the matchstick challenge</h1>
+    <hr/>
+    <h2>{{ prompt }}</h2>
+    <input type="number" v-model="numHouses" />
+    <p v-if="numHouses > 0">{{ numHouses }} of matchstick houses requires {{ matchhouses(numHouses) }} matchsticks.</p>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script>
+// BIG NOTICE:
+// Only use vue-cli command of vue new *projectname*
+// DO NOT USE: npm install vue@latest
+// Don't know why, just don't use it.
+
+//OK, get this order down:
+// export default {
+//   data() {
+//      },   <-- now the comma
+//     methods: {
+//      }, <-- now the comma
+//     components: {
+//      }
+//  } <--close export default
+//
+//  If you have plain variables, they go in
+//   data() and use return { }
+
+// default, return, data(), components, methods are 
+// javascript quasi-objects
+
+export default {
+  data() {
+    return {
+      prompt: "Enter a number of matchstick houses: ",
+      numHouses: 0
+    }
+  },
+  methods: {
+    matchhouses (steps){
+      if(steps < 1){
+        return 0;
+      }else if(steps === 1){
+        return 6;
+      }else{
+        return 6 + ((steps - 1) * 5);
+      }
+
+    }
+  }
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+</script>
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+<style>
+#app {
+  background-color: ivory;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
 </style>
