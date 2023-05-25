ec1c8be78af6978247029da0f5857f92acd79250
tardisgallifrey <bigmanrunning@gmail.com>
2023-05-25 13:50:39 -0500

    update 05/25/2023

--- projecthouse/angular/houses/src/app/app.component.ts
+++ projecthouse/angular/houses/src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'houses';
+  //Do not forget this is typescript.
+  //Type everything
+  title: string = 'houses';
+  prompt: string = 'Enter a number of matchstick houses: ';
+  numHouses: number = 0;
+
+  matchhouses = (steps: number) => {
+      if(steps < 1){
+        return 0;
+      }else if(steps === 1){
+        return 6;
+      }else{
+        return 6 + ((steps - 1) * 5);
+      }
+  }
 }
