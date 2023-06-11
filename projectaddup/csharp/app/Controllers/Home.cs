428f422905de3174b0056ba08f77910cdaa329b5
tardisgallifrey <bigmanrunning@gmail.com>
2023-06-11 13:44:14 -0500

    csharp 06/11/2023

--- projectaddup/csharp/app/Controllers/Home.cs
+++ projectaddup/csharp/app/Controllers/Home.cs
@@ -1,19 +1,38 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using app.Models;
 
 namespace app.Controllers;
 
 public class Home : Controller
 {
-    private readonly ILogger<Home> _logger;
-
-    public Home(ILogger<Home> logger)
-    {
-        _logger = logger;
-    }
-
+   
+   //Index gets the number and sends it to Output
     public IActionResult Index()
     {
+        var myvars = new Vars();
+
+        return View(myvars);
+    }
+
+    public IActionResult Output(Vars model)
+    {
+        int answer=0;
+        ViewData["start_point"] = model.number;
+
+        if (ModelState.IsValid)
+        {
+            if(model.number < 1001){
+                for(int i=1; i <= model.number; i++){
+                    answer += i;
+                }
+                ViewData["number"] = answer;
+            }else{
+                ViewData["number"] = -1;
+            }
+        }
+
+        
         return View();
     }
 
