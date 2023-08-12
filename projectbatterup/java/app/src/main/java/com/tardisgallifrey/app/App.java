b4121d6316d3cce056de07b77effd152e211cb51
tardisgallifrey <bigmanrunning@gmail.com>
2023-08-12 15:06:43 -0500

    java correction 08/12/2023

--- projectbatterup/java/app/src/main/java/com/tardisgallifrey/app/App.java
+++ projectbatterup/java/app/src/main/java/com/tardisgallifrey/app/App.java
@@ -1,10 +1,7 @@
 package com.tardisgallifrey.app;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
+import java.util.Scanner;
 import java.util.Vector;
-
-
 
 public class App{
 
@@ -15,28 +12,45 @@ public class App{
      static int totalbats = 0;
      static double average = 0.0;
 
-    public static void main(String[] args)
-        throws IOException
-    {
-       
-      BufferedReader cin = new BufferedReader(
-            new InputStreamReader(System.in));
+    public static void main(String[] args){
 
-      System.out.println("Batter's average calculator");
-      System.out.println("Enter number of games in series...");
-      int numgames = Integer.parseInt(cin.readLine());
- 
+      var cin = new Scanner(System.in);
+      var series = new Vector<Game>();
+      int numgames = 0;
+
+      System.out.println("Batter's Average Calculator");
+      System.out.print("How many games are in the series: ");
+      numgames = cin.nextInt();
+
+      // I had a serious problem making the Vector
+      //work.  The for(Game game : series) would 
+      //only print out data from the final entry
+      //into game.hits, game.atbats when 
+      //game was istantiated above.
+      //
+      //game is an object. Each write to hits 
+      //and atbats was not overwriting the previous.
+      //I needed to RE_instantiate game each time
+      //I added a new set of game data.
+      //Instantion goes inside the for loop.
       for(int idx = 0; idx < numgames; idx++){
-        System.out.println("Enter hits...");
-        totalhits += Integer.parseInt(cin.readLine());
-        System.out.println("Enter at bats...");
-        totalbats += Integer.parseInt(cin.readLine());
+        var game = new Game();
+        System.out.print("Enter number of hits: ");
+        game.hits = cin.nextInt();
+        System.out.print("Enter number of at bats: ");
+        game.atbats = cin.nextInt();
+        series.add(game);
+      }
+
+      for(Game game : series){
+        totalhits += game.hits;
+        totalbats += game.atbats;
       }
 
       average = (double)totalhits/(double)totalbats;
-      System.out.format("Batter's average for "+numgames+"games is: %.3f\n", average);  
- 
+      System.out.format("The batter's average over "+numgames+" games is: %.3f\n", average);
+            
       cin.close();
- 
     }
+  
 }
